80160337e4ab649460c76d6f2c089ec805392ebb
Willy <neo.dwedaz@gmail.com>
2025-09-25 01:44:08 +0700

    disable routes

--- src/routes/backpack.php
+++ src/routes/backpack.php
@@ -15,19 +15,3 @@ use Dwedaz\TripayH2H\Http\Controllers\Admin\TripayPostpaidProductCrudController;
 */
 
 // Only register routes if Backpack is installed
-if (class_exists('Backpack\CRUD\BackpackServiceProvider')) {
-    Route::group([
-        'prefix' => config('backpack.base.route_prefix', 'admin'),
-        'middleware' => array_merge(
-            (array) config('backpack.base.web_middleware', 'web'),
-            (array) config('backpack.base.middleware_key', 'admin')
-        ),
-        'namespace' => 'Dwedaz\TripayH2H\Http\Controllers\Admin',
-    ], function () {
-        // Tripay Prepaid Products - Read Only CRUD
-        Route::crud('tripay/prepaid-products', 'TripayPrepaidProductCrudController');
-
-        // Tripay Postpaid Products - Read Only CRUD
-        Route::crud('tripay/postpaid-products', 'TripayPostpaidProductCrudController');
-    });
-}
